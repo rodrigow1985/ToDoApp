caa2c62aab42eb15ceffae0d693d04e1057b2a1b
Rodrigo Weichert <rodrigoweichert@gmail.com>
2022-07-02 00:16:12 -0300

    Actualización en home, separé en componentes a la lista de tareas y la tarea en sí

--- containers/screens/home/index.js
+++ containers/screens/home/index.js
@@ -1,21 +1,11 @@
 import React, { Component, useState, useEffect } from 'react';
 import {
   View,
-  Text,
-  Button,
-  TouchableOpacity,
-  StyleSheet,
-  Image,
-  FlatList,
-  ActivityIndicator,
-  Box
+  StyleSheet
 } from 'react-native';
-import { getTasks } from '../../../data/api.js';
 // import FontAwesome, { Icons } from 'react-native-fontawesome';
 
-function HomeScreen ({ navigation }) {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
+import TaskList from "../../components/Taskslist";
 
   /*static navigationOptions = ({ navigation }) => {
     const { params = {} } = navigation.state;
@@ -23,58 +13,16 @@ function HomeScreen ({ navigation }) {
         title: 'Este es un título'
     };
   };*/
-
-  const loadTasks = async () => {
-    try {
-      const tasks = await getTasks();
-      setData(tasks);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-    
-    /*const apiData = await getTasks();
-    //console.log(apiData)
-    setData(apiData);
-    console.log('data');
-    console.log(data);
-    setLoading(false);*/
-  }
-
-  useEffect(() => {
-    loadTasks();
-    console.log("called");
-  }, []);
-
-  const onFlatListEmpty = () => {
-    return (
-      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
-          No Data Found in FlatList
-        </Text>
-      </View>
-    );
-  }
-
+  
+const HomeScreen = () => {
   return (
-    <View style={{ flex: 1, padding: 24 }}>
-      {isLoading ? <ActivityIndicator/> : (
-        <FlatList
-          data={data.tasks}
-          keyExtractor={(item) => item.id.toString()}
-          ListEmptyComponent={onFlatListEmpty}
-          renderItem={({ item }) => (
-            <Text>{item.title}, {item.done}</Text>
-          )}
-        />
-      )}
+    <View>
+      <TaskList />
     </View>
   );
-}
+};
 
 export default HomeScreen
-
 
 const styles = StyleSheet.create({
 

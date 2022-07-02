a6e82f23f73bad1374a95b6609df2983360cd3e9
Rodrigo Weichert <rodrigoweichert@gmail.com>
2022-07-02 00:12:58 -0300

    TaskItem.js v1.0.0

---
+++ containers/components/TaskItem.js
@@ -0,0 +1,53 @@
+
+import React, { useState } from "react";
+import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+import { useNavigation } from "@react-navigation/native";
+
+const TaskItem = ({ task, handleDelete }) => {
+    const navigation = useNavigation();
+    const [toggleCheckBox, setToggleCheckBox] = useState(false)
+    return (
+      <View style={styles.itemContainer}>
+        <TouchableOpacity
+          //onPress={() => navigation.navigate("TaskFormScreen", { id: task.id })}
+        >
+            <BouncyCheckbox
+                size={25}
+                fillColor="red"
+                unfillColor="#FFFFFF"
+                text={task.title}
+                iconStyle={{ borderColor: "red" }}
+                //textStyle={{ fontFamily: "JosefinSans-Regular" }}
+                onPress={(isChecked) => {setToggleCheckBox(isChecked)}}
+            />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ backgroundColor: "#ee5253", padding: 7, borderRadius: 5 }}
+          //onPress={() => handleDelete(task.id)}
+        >
+          <Text style={{ color: "#fff" }}>Delete</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+const styles = StyleSheet.create({
+    itemContainer: {
+      //backgroundColor: "#333333",
+      padding: 20,
+      marginVertical: 8,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      borderRadius: 5,
+    },
+    itemTitle: {
+      //color: "#ffffff",
+    },
+    checkbox: {
+        alignSelf: "center",
+    },
+  });
+
+export default TaskItem;
